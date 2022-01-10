be16773d60737b8a7be26d545b2fd242ed88b0c1
lety11 <sahabaz6296@gmail.com>
2022-01-11 00:19:37 +0530

    lety

---
+++ script.js
@@ -0,0 +1,13 @@
+var a=prompt("Enter UserName");
+//  var b=prompt("Enter password");
+  	if(a== "Lety")
+	//   && b=="2000"
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
